4af76a4a20936d7d4e36e99200ae0525af6193f5
kiiiiiii11 <anupapyaemkamol995@gmail.com>
2025-02-21 13:35:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebas
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqZRDgeN5itgNsX3lJIWP4e0djVfpInwk",
-  authDomain: "what-do-you-want-tell-teacher.firebaseapp.com",
-  databaseURL: "https://what-do-you-want-tell-teacher-default-rtdb.firebaseio.com",
-  projectId: "what-do-you-want-tell-teacher",
-  storageBucket: "what-do-you-want-tell-teacher.firebasestorage.app",
-  messagingSenderId: "614070987596",
-  appId: "1:614070987596:web:3639b0defdb33cc0bcfde9",
-  measurementId: "G-ZY48RBELXE"
+  apiKey: "AIzaSyARkljI4btWYdiZ2XTYO-2f6NtMKjAQZ8M",
+  authDomain: "what-do-you-want-tell-teacher2.firebaseapp.com",
+  databaseURL: "https://what-do-you-want-tell-teacher2-default-rtdb.firebaseio.com",
+  projectId: "what-do-you-want-tell-teacher2",
+  storageBucket: "what-do-you-want-tell-teacher2.firebasestorage.app",
+  messagingSenderId: "130178760324",
+  appId: "1:130178760324:web:b4e91e8b7e4db831d80cd1",
+  measurementId: "G-LLY0Z83VE3"
 };
 
 // Initialize Firebase
